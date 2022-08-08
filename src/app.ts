5fdf077dfe82c8889ae58d5f898719991b556f82
Denis Valladares <denisvalladares402@gmail.com>
2022-08-07 22:45:36 -0600

    fix cors

--- src/app.ts
+++ src/app.ts
@@ -2,14 +2,16 @@ import { env } from './config'
 import express from 'express'
 import mainRoutes from './routes'
 import morgan from 'morgan'
+import cors from 'cors'
 const app = express()
 
 // middelware
+app.use(cors())
 app.use(morgan('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 // config routes here
-app.use(env.context, mainRoutes)
+app.use(env.context + 'api', mainRoutes)
 
 export default app
