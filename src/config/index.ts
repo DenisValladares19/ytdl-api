3ea7eebbeff9e155dc862404284d9f5f3ba60931
Denis Valladares <denisvalladares402@gmail.com>
2022-08-07 22:46:05 -0600

    add key and base url for api consume

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,4 +1,10 @@
 export const env = {
     port: process.env.PORT || 8086,
     context: process.env.CONTEXT || '/',
+    YOUTUBE_API_KEY:
+        process.env.YOUTUBE_API_KEY ||
+        'AIzaSyARKYo3bevZmjgZExCDxJZdQ52jpvOIyfQ',
+    BASE_URL_YOUTUBE:
+        process.env.BASE_URL_YOUTUBE ||
+        'https://www.googleapis.com/youtube/v3/',
 }
