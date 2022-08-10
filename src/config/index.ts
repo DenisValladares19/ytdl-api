da6ba9c4f6b181fd6244a4cc5040d3bb1e138b95
Denis Valladares <denisvalladares402@gmail.com>
2022-08-09 22:58:43 -0600

    remove key

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,9 +1,7 @@
 export const env = {
     port: process.env.PORT || 8086,
     context: process.env.CONTEXT || '/',
-    YOUTUBE_API_KEY:
-        process.env.YOUTUBE_API_KEY ||
-        'AIzaSyARKYo3bevZmjgZExCDxJZdQ52jpvOIyfQ',
+    YOUTUBE_API_KEY: process.env.YOUTUBE_API_KEY || '',
     BASE_URL_YOUTUBE:
         process.env.BASE_URL_YOUTUBE ||
         'https://www.googleapis.com/youtube/v3/',
