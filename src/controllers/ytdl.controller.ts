646b1e4620b968aa084e5b3740f993009f73e99c
Denis Valladares <denisvalladares402@gmail.com>
2022-09-11 19:25:26 -0600

    test endpoint

--- src/controllers/ytdl.controller.ts
+++ src/controllers/ytdl.controller.ts
@@ -9,12 +9,13 @@ class YtdlController {
             if (!req.body.url) {
                 throw new Error('Url is required')
             }
-            const info = await YtdlService.getInfo(req.body.url)
+            const { formats } = await YtdlService.getInfo(req.body.url)
+            const result = formats.filter((format) => format.hasAudio)
 
             res.status(200).json({
                 status: 200,
                 message: 'Se obtuvo la información exitosamente',
-                data: info,
+                data: result,
             })
         } catch (error) {
             console.log('this is error => ', error)
